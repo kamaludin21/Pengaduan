8c4a945991db7d69940df29927b2c27b833738be
kamaludin21 <kamal.zyel@gmail.com>
2019-05-28 21:34:18 +0700

    Step 3 Search view, detail activity

--- app/src/main/java/com/example/pengaduan/HomeFragment.java
+++ app/src/main/java/com/example/pengaduan/HomeFragment.java
@@ -5,21 +5,55 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.pengaduan.main.MainAdapter;
+import com.example.pengaduan.main.MainPresenter;
+import com.example.pengaduan.main.MainView;
+
+import java.util.List;
 
 import static maes.tech.intentanim.CustomIntent.customType;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements MainView {
 
+    RecyclerView recyclerView;
+    SwipeRefreshLayout refreshLayout;
     FloatingActionButton add;
+
+    MainPresenter mainPresenter;
+    MainAdapter mainAdapter;
+    MainAdapter.ItemCLickListener itemCLickListener;
+    List<FormModel> form;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        add = (FloatingActionButton) v.findViewById(R.id.fab_add);
+        add = v.findViewById(R.id.fab_add);
+        recyclerView = v.findViewById(R.id.recycler_view);
+        refreshLayout = v.findViewById(R.id.swipe);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mainPresenter = new MainPresenter(this);
+        mainPresenter.getData();
+
+        refreshLayout.setOnRefreshListener(
+                () -> mainPresenter.getData()
+        );
+
+        itemCLickListener = (((view, position) -> {
+            String kategori = form.get(position).getKategori();
+            Toast.makeText(getActivity(), kategori, Toast.LENGTH_SHORT).show();
+        }));
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,4 +74,29 @@ public class HomeFragment extends Fragment {
 
     }
 
+    @Override
+    public void showLoading() {
+        refreshLayout.setRefreshing(true);
+
+    }
+
+    @Override
+    public void hideLoading() {
+        refreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onGetResult(List<FormModel> forms) {
+        mainAdapter = new MainAdapter(getActivity(),forms, itemCLickListener);
+        mainAdapter.notifyDataSetChanged();
+        recyclerView.setAdapter(mainAdapter);
+
+        form = forms;
+
+    }
+
+    @Override
+    public void onErrorLoading(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
 }
