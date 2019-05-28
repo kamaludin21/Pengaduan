fd440edc1e750ac67e6633d251af345a15047e2a
kamaludin21 <kamal.zyel@gmail.com>
2019-05-28 14:19:44 +0700

    Done, Testing again to see what's function failure

--- app/src/main/java/com/example/pengaduan/FormActivity.java
+++ app/src/main/java/com/example/pengaduan/FormActivity.java
@@ -1,6 +1,7 @@
 package com.example.pengaduan;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,14 +10,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FormActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText et_nama, et_nim, et_mail, et_text;
-    Spinner kategori;
+    Spinner sp_kategori;
     Button kirim;
 
     ProgressDialog progressDialog;
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,7 @@ public class FormActivity extends AppCompatActivity implements AdapterView.OnIte
         et_nim = findViewById(R.id.nim);
         et_mail = findViewById(R.id.email);
         et_text = findViewById(R.id.et_text);
-        kategori = findViewById(R.id.kategori);
+        sp_kategori = findViewById(R.id.kategori);
         kirim = findViewById(R.id.kirim);
 
         progressDialog = new ProgressDialog(this);
@@ -43,15 +50,64 @@ public class FormActivity extends AppCompatActivity implements AdapterView.OnIte
         kirim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveForm();
+                String nama = et_nama.getText().toString().trim();
+                String nim = et_nim.getText().toString().trim();
+                String email = et_mail.getText().toString().trim();
+                String kategori = sp_kategori.getSelectedItem().toString();
+                String text = et_text.getText().toString().trim();
+
+                if(nama.isEmpty()) {
+                    et_nama.setError("Tidak boleh kosong");
+                } else if (nim.isEmpty()) {
+                    et_nim.setError("Tidak boleh kosong");
+                } else if (email.isEmpty()) {
+                    et_mail.setError("Tidak boleh kosong");
+                } else if (text.isEmpty()) {
+                    et_text.setError("Tidak boleh kosong");
+                } else {
+                    saveForm(nama, nim, email, kategori, text);
+                }
             }
         });
 
     }
 
-    private void saveForm() {
+    private void saveForm(final String nama, final String nim, final String email, final String kategori, final String text) {
 
         progressDialog.show();
+
+        apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<FormModel> call = apiInterface.saveForm(nama, nim, email, kategori, text);
+
+        call.enqueue(new Callback<FormModel>() {
+            @Override
+            public void onResponse(@NonNull Call<FormModel> call,@NonNull Response<FormModel> response) {
+                progressDialog.dismiss();
+
+                if (response.isSuccessful() && response.body() != null) {
+
+                    Boolean success = response.body().getSuccess();
+                    if(success) {
+                        Toast.makeText(FormActivity.this, response.body().getMessage(),Toast.LENGTH_SHORT).show();
+                        finish();
+//                      // back to main activity
+                    } else {
+                        Toast.makeText(FormActivity.this, response.body().getMessage(),Toast.LENGTH_SHORT).show();
+                        // Still in this activity
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<FormModel> call,@NonNull Throwable t) {
+
+                progressDialog.dismiss();
+                Toast.makeText(FormActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
